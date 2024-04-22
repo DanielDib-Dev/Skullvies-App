2c27462f4318eae9df7e7ca97cc65ca80efb52a4
Daniel Dib <138417200+DanielDib-Dev@users.noreply.github.com>
2024-04-22 02:51:12 -0300

    Dark-mode finalizado

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-tab3',
@@ -11,6 +12,28 @@ export class Tab3Page {
 
   constructor() {}
 
-  toggleDarkMode() {}
+  ngOnInit(): void {
+    this.checkAppMode();
+  }
+
+  async checkAppMode() {
+    const checkIsDarkMode = await Preferences.get({key: 'darkModeActivated'});
+    console.log(checkIsDarkMode);
+    checkIsDarkMode?.value == 'true'
+      ? (this.darkMode = true)
+      : (this.darkMode = false);
+    document.body.classList.toggle('dark', this.darkMode);
+  }
+
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark', this.darkMode);
+    if(this.darkMode) {
+      Preferences.set({key: 'darkModeActivated', value: 'true'}); 
+    } else {
+      Preferences.set({key: 'darkModeActivated', value: 'false'});
+    }
+  }
 
 }
