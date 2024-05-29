ae496e5a9ab8e53e1e56b01bf0b33a92414448bc
Daniel Dib <dan-dib@hotmail.com>
2024-05-29 05:21:04 -0300

    Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, 
+            IonicModule.forRoot(), 
+            AppRoutingModule, 
+            HttpClientModule, 
+            FormsModule,
+            AngularFireModule.initializeApp(environment.firebaseConfig),
+            AngularFireAuthModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
