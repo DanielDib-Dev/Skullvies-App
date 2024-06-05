fb1fb29923cb2490389c5677c24d1cdf237a588e
Daniel Dib <dan-dib@hotmail.com>
2024-06-05 04:06:10 -0300

    Atualização environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCdgZ3dKR0p8mvyQ6Vniw46W8g8TZjI8i4",
+    authDomain: "skullvies-unisuam.firebaseapp.com",
+    projectId: "skullvies-unisuam",
+    storageBucket: "skullvies-unisuam.appspot.com",
+    messagingSenderId: "595234350127",
+    appId: "1:595234350127:web:53a766879470d4822592da"
+  }
 };
